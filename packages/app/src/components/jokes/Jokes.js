e9248e113c1e5d801e265e2f10fc87ac79769fd0
Logvinov_fv <Logvinov_fv@ursip.ru>
2019-06-21 15:23:42 +0300

    Restoring lost methods on a new paradigm #8

--- packages/app/src/components/jokes/Jokes.js
+++ packages/app/src/components/jokes/Jokes.js
@@ -3,6 +3,7 @@ import { useSelector, useDispatch } from 'react-redux'
 import { DynamicModuleLoader } from 'redux-dynamic-modules'
 
 import JokesDuck, { selectors as jokesSelectors, actions as jokesActions } from './jokes-duck'
+import * as UserDuck from '../user/user-duck'
 import Loading from '../common/Loading'
 import List from './List'
 import Groups from './Groups'
@@ -11,7 +12,7 @@ import SearchInput from '../common/SearchInput'
 function Jokes() {
   const loading = useSelector(jokesSelectors.loading)
   const searchQuery = useSelector(jokesSelectors.searchQuery)
-
+  const user = useSelector(UserDuck.selectors.singleUser)
   const dispatch = useDispatch()
   const handleSearch = value => dispatch(jokesActions.search(value))
 
@@ -20,7 +21,7 @@ function Jokes() {
       <SearchInput mb={3} value={searchQuery} onChange={handleSearch} />
       <Groups mb={3} />
       {loading && <Loading overlay>Loading funniest jokes...</Loading>}
-      <List />
+      <List user={user} />
     </DynamicModuleLoader>
   )
 }
--- packages/app/src/components/jokes/List.js
+++ packages/app/src/components/jokes/List.js
@@ -1,17 +1,30 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import { useSelector, useDispatch } from 'react-redux'
 import { Flex } from '@ursip/design-system'
-
-import Item from './Item'
+import selectorOnId from '../../utils/selectorOnId'
+import Item from '../common/Item'
+import noop from '../../utils/noop'
+import Likes from '../likes/Likes'
+import Comments from '../comments/Comments'
 
 import * as JokesDuck from './jokes-duck'
+import { actions as jokesActions } from './jokes-duck'
 
 function List(props) {
   const jokes = useSelector(JokesDuck.selectors.getData)
   const seen = useSelector(JokesDuck.selectors.seenJokes)
+  const opened = useSelector(JokesDuck.selectors.openedJokes)
+
   const dispatch = useDispatch()
+  const setOpen = value => dispatch(jokesActions.setOpen(value))
+  const setClose = value => dispatch(jokesActions.setClose(value))
 
   const setSeen = id => () => dispatch(JokesDuck.actions.setSeen(id))
+  const selector = (id, opened, setOpen, setClose) => () => {
+    setSeen(id)
+    selectorOnId(id, opened, setOpen, setClose)
+  }
 
   return (
     <Flex flexDirection="column" {...props}>
@@ -19,14 +32,36 @@ function List(props) {
         <Item
           key={joke.id}
           mb={3}
-          header={`#${joke.id} ${joke.setup}`}
-          content={joke.punchline}
-          seen={seen.includes(joke.id)}
-          onClick={setSeen(joke.id)}
-        />
+          header={'#' + joke.id + ' ' + joke.setup}
+          transparent={seen.includes(joke.id)}
+          opened={opened.includes(joke.id)}
+          onClick={selector(joke.id, opened, setOpen, setClose)}
+          punchline={joke.punchline}
+        >
+          <Likes jokeId={joke.id.toString()} />
+          <Comments jokeId={joke.id.toString()} user={props.user} />
+        </Item>
       ))}
     </Flex>
   )
+}
+
+List.propTypes = {
+  jokes: PropTypes.array,
+  seen: PropTypes.array,
+  opened: PropTypes.array,
+  setSeen: PropTypes.func,
+  setOpen: PropTypes.func,
+  setClose: PropTypes.func,
+}
+
+List.defaultProps = {
+  jokes: [],
+  seen: [],
+  opened: [],
+  setSeen: noop,
+  setOpen: noop,
+  setClose: noop,
 }
 
 export default List
